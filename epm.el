d39505f3025a8981388ccc945a764cf4aa616203
Chunyang Xu <xuchunyang.me@gmail.com>
2016-05-17 17:05:12 +0800

    Add command to display info on a package

--- epm.el
+++ epm.el
@@ -41,5 +41,54 @@
 (defun epm-delete (pkg-name)
   (package-delete (cadr (assoc (intern pkg-name) package-alist))))
 
+(defun epm-info (pkg-name)
+  ;; For feeding `package--builtins'
+  (require 'finder-inf nil t)
+  (let* ((pkg (intern pkg-name))
+         (desc (or
+                (cadr (assq pkg package-alist))
+                (let ((built-in (assq pkg package--builtins)))
+                  (if built-in
+                      (package--from-builtin built-in)
+                    (cadr (assq pkg package-archive-contents))))))
+         (pkg-dir (if desc (package-desc-dir desc)))
+         (reqs (if desc (package-desc-reqs desc)))
+         (version (if desc (package-desc-version desc)))
+         (archive (if desc (package-desc-archive desc)))
+         (extras (and desc (package-desc-extras desc)))
+         (homepage (cdr (assoc :url extras)))
+         (keywords (if desc (package-desc--keywords desc)))
+         (built-in (eq pkg-dir 'builtin))
+         (installable (and archive (not built-in)))
+         (status (if desc (package-desc-status desc) "orphan"))
+         (signed (if desc (package-desc-signed desc))))
+    (princ pkg-name)
+    (princ " is ")
+    (princ (if (memq (aref status 0) '(?a ?e ?i ?o ?u)) "an " "a "))
+    (princ status)
+    (princ " package.\n\n")
+    ;; Status
+    (cond (built-in
+           (princ "  Status: Built-In"))
+          (pkg-dir
+           (princ "  Status: Installed"))
+          (installable
+           (princ "  Status: Not installed"))
+          (t
+           (princ (format "  Status: %s" (capitalize status)))))
+    (princ "\n")
+    ;; Archive
+    (unless (and pkg-dir (not archive)) ; Installed pkgs don't have archive.
+      (princ (format " Archive: %s" (or archive "n/a")))
+      (princ "\n"))
+    ;; Version
+    (when version
+      (princ (format " Version: %s\n" (package-version-join version))))
+    (when desc
+      (princ (format " Summary: %s\n" (package-desc-summary desc))))
+    (when homepage
+      (princ (format "Homepage: %s\n" homepage)))
+    (princ "\n")))
+
 (provide 'epm)
 ;;; epm.el ends here
