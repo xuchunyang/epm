6375ddbf93c5f25647f6ebb25b54045b3c93a5be
Xu Chunyang <mail@xuchunyang.me>
2019-05-09 12:43:15 +0800

    Update the example .epm.el

--- .epm.el
+++ .epm.el
@@ -3,10 +3,8 @@
 ;; This file is supposed to bootstrap `package.el', refer your own
 ;; init file to find out what settings to populate this file.
 
-;; Emacs China ELPA mirror <http://elpa.emacs-china.org/> is faster in China
-(setq package-archives '(("gnu"   . "http://elpa.zilongshanren.com/gnu/")
-                         ("melpa" . "http://elpa.zilongshanren.com/melpa/")
-                         ("org"   . "http://elpa.zilongshanren.com/org/")))
+(setq package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
 
 ;; This is mandatory
 (package-initialize)
